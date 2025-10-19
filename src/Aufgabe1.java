a5aeaa586eb6e3bfbcaf0b79af37855304bd8061
Victor Carabulea <carabuleavictor26@gmail.com>
2025-10-20 02:20:41 +0300

    methode4

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -45,7 +45,34 @@ public static class Aufgabe1 {
         return noten;
     }
 
+    public int max_aufrunden_note(int[] noten){
+        int max_note = noten[0], aufrundung = 0;
 
+
+        for(int i = 0; i < noten.length; i++){
+            if(noten[i] > 100) noten[i] = 100;
+
+            if(noten[i] < 0) noten[i] = 0;
+
+            if(noten[i] >= 38){
+                int x = 40;
+                while(x < noten[i]){
+                    x += 5;
+                }
+
+                if(x - noten[i] < 3) {
+                    if(aufrundung < x - noten[i]){
+                        aufrundung = x - noten[i];
+                        max_note = noten[i];
+                    }
+                    noten[i] = x;
+                }
+            }
+
+        }
+
+        return max_note;
+    }
 }
 
 
@@ -61,7 +88,7 @@ void main() {
     Aufgabe1 aufgabe1 = new Aufgabe1();
 
     System.out.println(aufgabe1.durchschnitt(noten));
-    noten = aufgabe1.aufrunden(noten);
+    noten = aufgabe1.fail_noten(noten);
 
     for(int note : noten){
         if(note != 0){
@@ -69,6 +96,8 @@ void main() {
             System.out.print(' ');
         }
     }
+
+    System.out.println(aufgabe1.max_aufrunden_note(noten));
 
 
 
