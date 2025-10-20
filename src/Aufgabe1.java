50687f55d151fc4505a9316f76a8eec451562034
Victor Carabulea <carabuleavictor26@gmail.com>
2025-10-20 13:35:05 +0300

    repair methode4, more examples in main

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -20,7 +20,7 @@ public static class Aufgabe1 {
             sum += note;
         }
 
-        return sum / noten.length;
+        return Math.floor(sum / noten.length * 100) / 100;
     }
 
     public int[] aufrunden(int[] noten){
@@ -46,30 +46,13 @@ public static class Aufgabe1 {
     }
 
     public int max_aufrunden_note(int[] noten){
-        int max_note = noten[0], aufrundung = 0;
+        int max_note = -1;
 
+        noten = aufrunden(noten);
 
-        for(int i = 0; i < noten.length; i++){
-            if(noten[i] > 100) noten[i] = 100;
-
-            if(noten[i] < 0) noten[i] = 0;
-
-            if(noten[i] >= 38){
-                int x = 40;
-                while(x < noten[i]){
-                    x += 5;
-                }
-
-                if(x - noten[i] < 3) {
-                    if(aufrundung < x - noten[i]){
-                        aufrundung = x - noten[i];
-                        max_note = noten[i];
-                    }
-                    noten[i] = x;
-                }
+        for(int note : noten){
+                if(note > max_note) max_note = note;
             }
-
-        }
 
         return max_note;
     }
@@ -77,26 +60,39 @@ public static class Aufgabe1 {
 
 
 void main() {
-    int[] noten = {84,100,38,20,60,65,62};
+    int[] noten = {29, 37, 38, 41, 84, 67};
 
 
-//    for(int note : noten){
-//        System.out.print(note);
-//        System.out.print(' ');
-//    }
+    for(int note : noten){
+        System.out.print(note);
+        System.out.print(' ');
+    }
 
     Aufgabe1 aufgabe1 = new Aufgabe1();
 
-    System.out.println(aufgabe1.durchschnitt(noten));
-    noten = aufgabe1.fail_noten(noten);
+    int[] failnoten = aufgabe1.fail_noten(noten);
 
-    for(int note : noten){
+    System.out.print('\n');
+    for(int note : failnoten){
         if(note != 0){
             System.out.print(note);
             System.out.print(' ');
         }
     }
 
+    System.out.print('\n');
+    System.out.println(aufgabe1.durchschnitt(noten));
+
+    int[] abgerundet = aufgabe1.aufrunden(noten);
+
+    for(int note : abgerundet){
+        if(note != 0){
+            System.out.print(note);
+            System.out.print(' ');
+        }
+    }
+
+    System.out.print('\n');
     System.out.println(aufgabe1.max_aufrunden_note(noten));
 
 
